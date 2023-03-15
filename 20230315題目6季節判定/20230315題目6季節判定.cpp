0b37642c096d30a57200dd52f196bb3a70defa0e
rober <rober@LAPTOP-9BJTS009>
2023-03-15 21:10:13 +0800

    加入專案檔案。

---
+++ 20230315題目6季節判定/20230315題目6季節判定.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdlib.h>    
+#include <stdio.h>    
+
+int main() {
+    int x;
+    scanf("%d", &x);
+    switch (x) {
+    case 3:
+    case 4:
+    case 5:
+        printf("Spring\n");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        printf("Summer\n");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        printf("Autumn\n");
+        break;
+    case 12:
+    case 1:
+    case 2:
+        printf("Winter\n");
+        break;
+    }
+}
